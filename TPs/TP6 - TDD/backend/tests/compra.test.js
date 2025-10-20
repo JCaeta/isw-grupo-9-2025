4bf2ae83a0e1c328f99d9119d2d0e5ce9a5137e0
ignapb02 <igna.aldao@gmail.com>
2025-10-19 21:26:29 -0300

    tests: se agrega test para verificar precio de entrada

--- TPs/TP6 - TDD/backend/tests/compra.test.js
+++ TPs/TP6 - TDD/backend/tests/compra.test.js
@@ -9,4 +9,16 @@ describe("Compra de entradas - Grupo 9", () => {
     expect(entrada.edad).toBe(25);
     expect(entrada.tipoPase).toBe("VIP");
   });
+
+  test("Test para verificar el precio", () => {
+    const entradaVipAdulto = new Entrada(25, "VIP");
+    const entradaRegularNiño = new Entrada(8, "Regular");
+    const entradaGratis = new Entrada(2, "Regular");
+    const entradaMayor = new Entrada(65, "VIP");
+
+    expect(entradaVipAdulto.calcularPrecio()).toBe(10000);
+    expect(entradaRegularNiño.calcularPrecio()).toBe(2500); // mitad
+    expect(entradaGratis.calcularPrecio()).toBe(0);
+    expect(entradaMayor.calcularPrecio()).toBe(5000); // mitad del VIP
+  });
 });
