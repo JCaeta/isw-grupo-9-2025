a16f810d70933e8497a96c68e27131a093eeef94
ignapb02 <igna.aldao@gmail.com>
2025-10-19 21:44:36 -0300

    Implementacion: se agrega logica para validar dia de compra de entrada

--- TPs/TP6 - TDD/backend/src/Compra.js
+++ TPs/TP6 - TDD/backend/src/Compra.js
@@ -6,6 +6,27 @@ class Compra {
     this.entradas = entradas;
     this.metodoPago = metodoPago;
   }
+
+  validarFecha() {
+    const diaSemana = this.fechaVisita.getDay(); // 0=domingo, 1=lunes, ...
+    const festivos = [
+      { dia: 25, mes: 12 },
+      { dia: 1, mes: 1 }
+    ];
+
+    if (diaSemana === 1) {
+      throw new Error("Parque cerrado los lunes");
+    }
+
+    const esFestivo = festivos.some(
+      f =>
+        f.dia === this.fechaVisita.getDate() &&
+        f.mes === this.fechaVisita.getMonth() + 1
+    );
+    if (esFestivo) {
+      throw new Error("Parque cerrado por feriado");
+    }
+  }
 }
 
 module.exports = Compra;
