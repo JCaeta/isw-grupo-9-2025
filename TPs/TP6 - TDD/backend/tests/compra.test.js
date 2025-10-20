517729099039f71cb72c34f52cb70569e0c8235e
ignapb02 <igna.aldao@gmail.com>
2025-10-19 21:20:35 -0300

    Test: se agrega test de crear una entrada

---
+++ TPs/TP6 - TDD/backend/tests/compra.test.js
@@ -0,0 +1,12 @@
+const Usuario = require("../src/models/Usuario");
+const Entrada = require("../src/models/Entrada");
+const Compra = require("../src/Compra");
+
+describe("Compra de entradas - Grupo 9", () => {
+    test("Test para crear una entrada", () => {
+    const entrada = new Entrada(25, "VIP");
+    expect(entrada).toBeInstanceOf(Entrada);
+    expect(entrada.edad).toBe(25);
+    expect(entrada.tipoPase).toBe("VIP");
+  });
+});
